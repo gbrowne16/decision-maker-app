8b92f47f8efa22b294b19cc8ff5475f053aacfb5
gbrowne16 <gbrowne28@students.stab.org>
2024-09-26 11:55:01 -0400

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,9 +1,11 @@
 
+//this is the function that determines what will be shown on the screen
 function getHouse(inputBudget, numPeople, life){
 
 
 
-// put pic into html and then use variables to insert when needed
+//turns the pictures into variables
+
 var closet = "Sourya_Venumbaka_before_1_tagged.webp";
 var stables = "download-4.jpg";  
 var niceFarm = "61c82819910c6c793c6a7cb1efa39d95.jpg";
@@ -12,6 +14,8 @@ var pent = "download-1.jpg";
 var apartS = "download-3.jpg";
 var apartB = "download-2.jpg";
 var cabin = "download.jpg";
+
+//a variable to define the output
 var house;
 
 if ((inputBudget<100000) && life== "City"){
@@ -38,14 +42,15 @@ else if((inputBudget<1000000) && life!="Rural"&& numPeople>=5){
 else if((inputBudget>=1000000) && life=="Rural"){
    house = niceFarm
 }
-else {
+else if((inputBudget>=1000000)&& life!="Rural"){
    house = pent
 }
+//returns what house will be shown on the screen
 return house
 }
 
 
-
+//this function displays the house determined above
 function displayHouse(){
      
 var yourHouse = getHouse(document.getElementById("budget").value, document.getElementById("people").value, document.getElementById("lifestyle").value);
