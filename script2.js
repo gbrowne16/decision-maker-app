66f7379c06afa1f7cd582c04aa9766d1eff1f6ee
Ned27 <eng27@students.stab.org>
2024-09-26 11:45:06 -0400

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,47 +1,54 @@
-var age
-function getHouse(){
-age =6
 
-var inputBudget=  document.getElementById("budget").value;
-var numPeople=  document.getElementById("people").value;
-var life=  document.getElementById("lifestyle").value;
-var outputBox=  document.getElementById("outputBox").value;
+function getHouse(inputBudget, numPeople, life){
+
+
+
 // put pic into html and then use variables to insert when needed
-var closet ;
-var stables ;  
-var niceFarm;
-var modFarm;
-var pent;
-var apartS;
-var apartB;
-var cabin;
+var closet = "Sourya_Venumbaka_before_1_tagged.webp";
+var stables = "download-4.jpg";  
+var niceFarm = "61c82819910c6c793c6a7cb1efa39d95.jpg";
+var modFarm = "images.jpg";
+var pent = "download-1.jpg";
+var apartS = "download-3.jpg";
+var apartB = "download-2.jpg";
+var cabin = "download.jpg";
+var house;
 
-if (inputBudget<100000 && life== "City"){
-    closet
+if ((inputBudget<100000) && life== "City"){
+    house = closet
 }
-else if (inputBudget>1000000 && life=="Rural"){
-    stables
+else if ((inputBudget<100000) && life=="Rural"){
+    house = stables
 }
-else if(inputBudget>100000000 && life=="Rural"&& numPeople<5){
-    modFarm
+else if((inputBudget<1000000) && life=="Rural" && numPeople<5){
+    console.log("moderate farm house");
+    house = modFarm
 }
-else if(inputBudget>100000000 && life=="Rural"&& numPeople>=5){
-    cabin
+else if((inputBudget<1000000) && life=="Rural"&& numPeople>=5){
+    console.log("cabin")
+    house = cabin
 }
-else if(inputBudget>100000000 && life!="Rural"&& numPeople<5){
-    apartS
+else if((inputBudget<1000000) && life!="Rural"&& numPeople<5){
+    house = apartS
 }
-else if(inputBudget>100000000 && life!="Rural"&& numPeople>=5){
-    apartB
+else if((inputBudget<1000000) && life!="Rural"&& numPeople>=5){
+    house = apartB
 }
-else if(inputBudget>100000000 && life=="Rural"&& numPeople<5){
-    modFarm
+
+else if((inputBudget>=1000000) && life=="Rural"){
+   house = niceFarm
 }
-else if(inputBudget<=100000000 && life=="Rural"){
-    niceFarm
+else {
+   house = pent
 }
-else if(inputBudget<=100000000 && life!="Rural"){
-    niceFarm
-    console.log(h)
+return house
 }
+
+
+
+function displayHouse(){
+     
+var yourHouse = getHouse(document.getElementById("budget").value, document.getElementById("people").value, document.getElementById("lifestyle").value);
+console.log(yourHouse);  
+document.getElementById("houseImage").src = yourHouse;
 }
